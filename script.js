90448ac6f37d06098a221787d9ed8550382d3e27
Ritesh Saini <36216813+heyitsritesh@users.noreply.github.com>
2024-08-27 12:11:50 +0530

    Finished first stage of the project: - Added Gameboard IIFE that creates 9 squares with unique CSS IDs and adds them to the HTML of the page when the 'start' button is clicked. - Added createPlayer factory function to created players. - Added Game IIFE that will control the game. It will create players with fixed symbols and store them to an array, render gameboard, and attach even listener to each square which shows an alert when clicked (for testing). - Added event listener to the start button.

---
+++ script.js
@@ -0,0 +1,49 @@
+const Gameboard = (() => {
+    let gameboard = ['', '', '', '', '', '', '', '', ''];
+    const render = () => {
+        let boardHTML = '';
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div class="square" id="square-${index}">${square}</div>`;
+        });
+        document.querySelector('#gameboard').innerHTML = boardHTML;
+    };
+
+    return { render };
+})();
+
+const createPlayer = (name, mark) => {
+    return {
+        name,
+        mark,
+    };
+};
+
+const Game = (() => {
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;
+
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector('#player1').value, 'X'),
+            createPlayer(document.querySelector('#player2').value, 'O'),
+        ];
+        currentPlayerIndex = 0;
+        gameOver = false;
+        Gameboard.render();
+        const squares = document.querySelectorAll('.square');
+        squares.forEach(square => {
+            square.addEventListener('click', handleClick);
+        });
+    };
+
+    const handleClick = event => {
+        alert(parseInt(event.target.id.split('-')[1]));
+    };
+    return { start, handleClick };
+})();
+
+const startButton = document.querySelector('#start-button');
+startButton.addEventListener('click', () => {
+    Game.start();
+});
