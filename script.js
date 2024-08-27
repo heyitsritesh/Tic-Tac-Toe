9b3b1fba27aa0f9a04dcf74fc8d8f50000bc0ab0
Ritesh Saini <36216813+heyitsritesh@users.noreply.github.com>
2024-08-27 14:08:07 +0530

    Added several changes: - Moved the loop that adds event listeners to all the squares to the Gameboard IIFE. - Improved handleClick function to add player symbol and switch the player on click. - Added logic for the reset button

--- script.js
+++ script.js
@@ -6,9 +6,20 @@ const Gameboard = (() => {
             boardHTML += `<div class="square" id="square-${index}">${square}</div>`;
         });
         document.querySelector('#gameboard').innerHTML = boardHTML;
+        const squares = document.querySelectorAll('.square');
+        squares.forEach(square => {
+            square.addEventListener('click', Game.handleClick);
+        });
     };
 
-    return { render };
+    const update = (index, value) => {
+        gameboard[index] = value;
+        render();
+    };
+
+    const getGameboard = () => gameboard;
+
+    return { render, update, getGameboard };
 })();
 
 const createPlayer = (name, mark) => {
@@ -31,19 +42,32 @@ const Game = (() => {
         currentPlayerIndex = 0;
         gameOver = false;
         Gameboard.render();
-        const squares = document.querySelectorAll('.square');
-        squares.forEach(square => {
-            square.addEventListener('click', handleClick);
-        });
     };
 
     const handleClick = event => {
-        alert(parseInt(event.target.id.split('-')[1]));
+        let index = parseInt(event.target.id.split('-')[1]);
+
+        if (Gameboard.getGameboard()[index] !== '') return;
+
+        Gameboard.update(index, players[currentPlayerIndex].mark);
+        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
     };
-    return { start, handleClick };
+
+    const restart = () => {
+        for (let i = 0; i < 9; i++) {
+            Gameboard.update(i, '');
+        }
+    };
+
+    return { start, handleClick, restart };
 })();
 
 const startButton = document.querySelector('#start-button');
 startButton.addEventListener('click', () => {
     Game.start();
+});
+
+const restartButton = document.querySelector('#restart-button');
+restartButton.addEventListener('click', () => {
+    Game.restart();
 });
